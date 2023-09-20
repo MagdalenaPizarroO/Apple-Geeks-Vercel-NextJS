0e712a8e1d2a11a16d7b1e54d589c32bbfb151e1
MagdalenaPizarroO <mspizarro.ortega@gmail.com>
2023-09-19 21:08:34 -0300

    client and local server ok

---
+++ client/app/page.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+type pageProps = {};
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+const page: React.FC<pageProps> = async () => {
+  const res = await fetch("http://127.0.0.1:8080/api/home", { cache: "no-store" });
+  const users: User[] = await res.json();
+
+  return (
+    <>
+      <h1>Users</h1>
+      <table className='table table-bordered'>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
+export default page;
---
+++ server/README.md
@@ -0,0 +1,4 @@
+# IMPORTANTE
+
+Recordar eliminar la línea `debug=True`antes de desplegar la aplicación!  
+Esta línea se encuentra donde ejecuto mi aplicación Flask, app.run(); debo cambiarlo a `debug=False` o eliminarlo y dejar solo app.run()
