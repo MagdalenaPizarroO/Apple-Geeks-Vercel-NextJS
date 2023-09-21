b8026b56e532c8e8fb42c93c937dc2386b67b2e3
MagdalenaPizarroO <mspizarro.ortega@gmail.com>
2023-09-21 17:44:33 -0300

    everything works

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,16 @@ import React from "react";
 
 type pageProps = {};
 
-interface User {
+interface Usuario {
   id: number;
   nombre: string;
   correo: string;
+  apellido: string;
 }
 
 const page: React.FC<pageProps> = async () => {
-  const res = await fetch("http://127.0.0.1:8080/api/poblar_db", { cache: "no-store" });
-  const users: User[] = await res.json();
+  const res = await fetch("http://127.0.0.1:8080/api/ver_db", { cache: "no-store" });
+  const users: Usuario[] = await res.json();
 
   return (
     <>
@@ -20,6 +21,7 @@ const page: React.FC<pageProps> = async () => {
           <tr>
             <th>Name</th>
             <th>Email</th>
+            <th>Apellido</th>
           </tr>
         </thead>
         <tbody>
@@ -27,6 +29,7 @@ const page: React.FC<pageProps> = async () => {
             <tr key={user.id}>
               <td>{user.nombre}</td>
               <td>{user.correo}</td>
+              <td>{user.apellido}</td>
             </tr>
           ))}
         </tbody>
